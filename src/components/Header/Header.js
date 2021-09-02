c2089ed8be0dffcdb5547fcbc2d0e6bb243106b0
changhoetyng <hoetyng@gmail.com>
2021-09-03 00:47:46 +0800

    Header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,37 @@
-import './Header.css'
+import "./Header.css";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { Person, Mail } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function Header() {
+  const classes = useStyles();
   return (
-      <header className="Header">
-          <p style={{fontSize: 40}}>Header</p>
-      </header>
+    <header className="Header">
+      <div className="wrapper">
+        <div className="left">
+          <span>hoe</span>
+        </div>   
+        <div className="right">
+          <span>Home</span>
+          <span>About Me</span>
+          <span>Experience</span>
+          <span>Projects</span>
+          <span>Get In Touch</span>
+        </div>
+      </div>
+    </header>
   );
 }
 
